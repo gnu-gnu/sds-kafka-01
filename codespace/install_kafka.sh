2595c2059736d4875b27fdc4e6d8b497dc98b6a0
GNU Shim <geunwoo.j.shim@gmail.com>
2023-03-24 08:59:44 +0900

    Update install_kafka.sh

--- codespace/install_kafka.sh
+++ codespace/install_kafka.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-curl -L https://downloads.apache.org/kafka/3.4.0/kafka_2.13-3.4.0.tgz 
+curl -LO https://downloads.apache.org/kafka/3.4.0/kafka_2.13-3.4.0.tgz 
 tar -xvf kafka_2.13-3.4.0.tgz -C /workspace/sds-kafka-01
